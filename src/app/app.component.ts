6b06b11d6322ffc914ca4d659b62477732eaa9df
Patrick Yong <patrick@tentspark.com>
2017-06-10 13:50:11 +0800

    move code from app to hello-world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  label: string;
-  customer: ICustomer;
-
-  constructor() {
-    this.label = 'I am';
-    this.customer = {
-      name: 'Chris',
-      age: 12,
-      gender: 'male'
-    };
-
-  }
-
-  alert(param: string) {
-      alert(param);
-  }
-
-  // this is a property
-  get isMale() {
-    return this.customer.gender === 'male';
-  }
-
-  changeGender() {
-    this.customer.gender = this.customer.gender === 'male' ? 'female' : 'male';
-  }
-
-}
-
-interface ICustomer {
-  name: string;
-  age: number;
-  gender: string;
 }
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent implements OnInit {
-
-  constructor() { }
+  label: string;
+  customer: ICustomer;
 
   ngOnInit() {
   }
 
+  constructor() {
+    this.label = 'I am';
+    this.customer = {
+      name: 'Chris',
+      age: 12,
+      gender: 'male'
+    };
+
+  }
+
+  alert(param: string) {
+      alert(param);
+  }
+
+  // this is a property
+  get isMale() {
+    return this.customer.gender === 'male';
+  }
+
+  changeGender() {
+    this.customer.gender = this.customer.gender === 'male' ? 'female' : 'male';
+  }
+
+}
+
+interface ICustomer {
+  name: string;
+  age: number;
+  gender: string;
 }
