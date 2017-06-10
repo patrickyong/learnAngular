cd48af9e6fef0232bf777c4bee32c96d4f153cea
Patrick Yong <patrick@tentspark.com>
2017-06-10 11:48:58 +0800

    morning class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  label: string;
+  customer: ICustomer;
+  
+
+  constructor() {
+    this.label = 'my name';
+    
+    this.customer = {
+      name: 'Chris',
+      age: 12,
+      gender: 'male'
+    };
+
+  }
+
+  alert(param: string)
+  {
+      alert(param);
+  }
+
+  isMale()
+  {
+    return this.customer.gender == "male";
+  }
+
+  male()
+  {
+    this.customer.gender = "male";
+  }
+
+  female()
+  {this.customer.gender = "female";}
+}
+
+interface ICustomer {
+  name: string;
+  age: number;
+  gender: string;
 }
